2533721e802ea9b764388130f0db4b0bf2472a8f
juan-20 <juanalvesandrade@gmail.com>
2023-07-03 02:12:19 -0300

    changing title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Footer from '@/components/footer'
 import Hero from '@/components/hero'
 import Navbar from '@/components/navbar'
 import VideoPage from '@/app/videoPagination'
-import { VideoProps } from '@/types/movie'
-import getMovies from '@/utils/getVideos'
+import getVideos from '@/utils/getVideos'
+
 
 
 export default async function Home() {
-  const data = { result: await getMovies(1500) }
+  const data = { result: await getVideos(1500) }
+
 
   return (
     <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
--- src/utils/getVideos.ts
+++ src/utils/getVideos.ts
@@ -1,4 +1,4 @@
-export default async function getMovies(
+export default async function getVideos(
   time: number = 0,
   shouldError: boolean = false
 ) {
